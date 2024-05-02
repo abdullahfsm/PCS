00d8b9143c82cb4e1d57d0a0fce8899bd1ac80e7
abdffsm <abdffsm@localhost>
2024-05-02 15:21:22 -0500

    added workload reproduce

--- run_workload2.sh
+++ run_workload2.sh
@@ -2,14 +2,14 @@
 
 output_files=()
 
-for trace in {0e4a51,}
-# for trace in {0e4a51,6214e9,6c71a0,b436b2}
+# for trace in {0e4a51,}
+for trace in {0e4a51,6214e9,6c71a0,b436b2}
 do
 	for metric in {avg_jct_avg_pred_error,}
 	do
 		for var in {jct,bal,pred,}
 		do
-			python3 simulation/sim.py -scheduling_policy MCS -num_gpus 64 -output_file PCS_"$trace"_"$metric"_"$var".csv -trace trace_"$trace" -estimate 1 -MCS_config_file data/PCS_configs/PCS_config_"$trace"_"$metric"_"$var".pkl -num_apps 5
+			python3 simulation/sim.py -scheduling_policy MCS -num_gpus 64 -output_file PCS_"$trace"_"$metric"_"$var".csv -trace trace_"$trace" -estimate 1 -MCS_config_file data/PCS_configs/PCS_config_"$trace"_"$metric"_"$var".pkl -num_apps 50000000000
 			output_files+=(PCS_"$trace"_"$metric"_"$var".csv)
 			echo "$var" done
 		done
@@ -18,15 +18,15 @@ do
 	echo PCS done
 
 
-	for policy in {FIFO,SRSF,}
+	for policy in {FIFO,SRSF,AFS,}
 	do
-		python3 simulation/sim.py -scheduling_policy "$policy" -num_gpus 64 -output_file "$policy"_"$trace".csv -trace trace_"$trace" -estimate 1 -num_apps 5
+		python3 simulation/sim.py -scheduling_policy "$policy" -num_gpus 64 -output_file "$policy"_"$trace".csv -trace trace_"$trace" -estimate 1 -num_apps 500000000000
 		output_files+=("$policy"_"$trace".csv)
 		echo "$policy" done
 	done
 	
 done
 
-python3 simulation/utils/result_summary.py -fnames "${output_files[@]}"
+python3 simulation/utils/result_summary.py -fnames "${output_files[@]}" -normalize_jct
 
 
--- simulation/utils/result_summary.py
+++ simulation/utils/result_summary.py
@@ -19,16 +19,26 @@ def main(args):
             print(fname)
             fair_result_dictionary = results[fname]
 
+
+
+    min_avg_jct = min([avg_ACT(results[fname]) for fname in fnames]) if args.normalize_jct else 1
+    min_p90_jct = min([np.percentile(cdf_ACT(results[fname])[0], q=90) for fname in fnames]) if args.normalize_jct else 1
+    min_p95_jct = min([np.percentile(cdf_ACT(results[fname])[0], q=95) for fname in fnames]) if args.normalize_jct else 1
+    min_p99_jct = min([np.percentile(cdf_ACT(results[fname])[0], q=99) for fname in fnames]) if args.normalize_jct else 1
+
+
+
     for fname in fnames:
         perc = 95
 
         print(".................")
         print(".................")
         print(f"fname: {fname}")
-        print(f"avg JCT: {avg_ACT(results[fname])}")
-        print(f"p{90} JCT: {np.percentile(cdf_ACT(results[fname])[0], q=90)}")
-        print(f"p{95} JCT: {np.percentile(cdf_ACT(results[fname])[0], q=95)}")
-        print(f"p{99} JCT: {np.percentile(cdf_ACT(results[fname])[0], q=99)}")
+        print(f"avg JCT: {(round(avg_ACT(results[fname])/min_avg_jct, 2))}")
+        print(f"p{90} JCT: {round(np.percentile(cdf_ACT(results[fname])[0], q=90)/min_p90_jct,2)}")
+        print(f"p{95} JCT: {round(np.percentile(cdf_ACT(results[fname])[0], q=95)/min_p95_jct,2)}")
+        print(f"p{99} JCT: {round(np.percentile(cdf_ACT(results[fname])[0], q=99)/min_p99_jct,2)}")
+
         print(f"avg pred error: {avg_ACT_error(results[fname])}")
         print(
             f"p{90} pred error: {np.percentile(cdf_ACT_error(results[fname])[0], q=90)}"
@@ -52,6 +62,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-fnames", nargs="+", help="input filenames", type=str, required=True
     )
+    parser.add_argument('-normalize_jct', help = "normalize", action='store_true')
 
     args = parser.parse_args()
     main(args)
