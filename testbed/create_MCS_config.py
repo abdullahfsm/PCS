742fdcde2974bb8d7cc2f83de57b9a0c8d50f732
abdullah <abdullah@cs.tufts.edu>
2024-05-04 14:03:50 -0400

    added mcs

--- testbed/create_MCS_config.py
+++ testbed/create_MCS_config.py
@@ -3,7 +3,7 @@ from fractions import Fraction as frac
 
 # class_detail = {"num_classes": 3, "class_thresholds": [1718, 2757, float('inf')], "class_rates": [frac(941,1000),frac(7,125),frac(3,1000)]}
 # class_detail = {"num_classes": 2, "class_thresholds": [4790, float('inf')], "class_rates": [frac(23,25),frac(2,25)], "clip_demand_factor": 0.01, "delta": 0.01}
-class_detail = {"num_classes": 1, "class_thresholds": [float('inf')], "class_rates": [frac(25,25)], "clip_demand_factor": 0.01, "delta": 0.01}
+class_detail = {"num_classes": 1, "class_thresholds": [100,float('inf')], "class_rates": [frac(25,25)], "clip_demand_factor": 0.01, "delta": 0.01}
 
 file_name = "{}_{}_{}.pkl".format(
 	class_detail["num_classes"],
