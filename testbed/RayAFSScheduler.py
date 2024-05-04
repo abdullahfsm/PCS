f2141dbc3b333d4c5247f96e0675ca1a4c23ea9c
abdullah <abdullah@cs.tufts.edu>
2024-05-04 16:21:00 -0400

    added afs

--- testbed/RayAFSScheduler.py
+++ testbed/RayAFSScheduler.py
@@ -3,7 +3,7 @@ from RayGenericScheduler import RayAppGenericScheduler
 import os, sys
 from datetime import datetime, timedelta
 from time import sleep
-import numpy as np
+import math
 from functools import partial
 import copy
 
@@ -12,10 +12,12 @@ from AFSScheduler import AppAFSScheduler
 from common import Event, App, Job
 
 
+EPS=1e-7
+
 class RayAppAFSScheduler(RayAppGenericScheduler):
     """docstring for RayAppMCScheduler"""
-    def __init__(self, total_gpus, event_queue, app_list, app_info_fn="results.csv", suppress_print=True):
-        super(RayAppAFSScheduler, self).__init__(total_gpus, event_queue, app_list, app_info_fn, suppress_print=False)
+    def __init__(self, total_gpus, event_queue, app_list, prio_func, app_info_fn="results.csv", suppress_print=False, estimate=True):
+        super(RayAppAFSScheduler, self).__init__(total_gpus, event_queue, app_list, app_info_fn, suppress_print=suppress_print, estimate=estimate)
     
 
     def sim_estimate(self, app):
@@ -33,21 +35,12 @@ class RayAppAFSScheduler(RayAppGenericScheduler):
         snap_shot._app_id_to_allocation = copy.deepcopy(self._app_id_to_allocation)
 
         for virtual_app in snap_shot._active_apps+[copy.deepcopy(app)]:
-            
-
-            for attr in ['future', 'exec_func', 'trial_runner_queue']:
-                if hasattr(virtual_app, attr):
-                    delattr(virtual_app, attr)
-            
             snap_shot._app_list[virtual_app.app_id] = virtual_app
 
 
 
-        snap_shot._estimate = False
         snap_shot._suppress_print = True
         snap_shot._verbosity = 0
-        snap_shot._estimator = True
-
         snap_shot._init_time = self._init_time
         snap_shot._last_event_time = self._last_event_time
 
@@ -68,48 +61,63 @@ class RayAppAFSScheduler(RayAppGenericScheduler):
         app.update_estimates(snap_shot._app_list[app.app_id].start_time,
                             snap_shot._app_list[app.app_id].end_time)
 
-    def top_priority(self, current_allocation):
-        
-        while True:
-            a_star = np.random.choice(self._active_apps)
-            if a_star.demand > 0:
-                break
 
+    def alg_c_concept(self, total_gpus):
+        js = [m for m in self._active_apps]
+        for m in js:
+            m.tmp_gpus = 0
+        gpus = total_gpus
 
-        for app in self._active_apps:
-            if app.app_id == a_star.app_id or app.demand == 0:
-                continue
-
-            app_a, app_b = a_star, app
-
-            if current_allocation[app_a.app_id] == 0 and current_allocation[app_b.app_id] == 0:
-                if app_a.remaining_service < app_b.remaining_service:
-                    a_star = app_a
+        while gpus > 0 and len(js) > 0:
+            cand = js[0]
+            for m in js[1:]:
+                if self.compute_remaining_time(m, m.tmp_gpus) < self.compute_remaining_time(cand, cand.tmp_gpus):
+                    l, h = m, cand
                 else:
-                    a_star = app_b
-            else:
-
-                app_a_remaining_time = app_a.remaining_service/current_allocation[app_a.app_id] if current_allocation[app_a.app_id] > 0 else float('inf')
-                app_b_remaining_time = app_b.remaining_service/current_allocation[app_b.app_id] if current_allocation[app_b.app_id] > 0 else float('inf')
+                    l, h = cand, m
 
 
-                if app_a_remaining_time >= app_b_remaining_time:
-                    app_a, app_b = app_b, app_a
-
-                    # throughput with current allocation
-                    p_a, p_b = current_allocation[app_a.app_id], current_allocation[app_b.app_id]
-
-                    # throughput with extra GPU
-                    p_a_p = p_a+1 if current_allocation[app_a.app_id] < app_a.demand else p_a
-                    p_b_p = p_b+1 if current_allocation[app_b.app_id] < app_b.demand else p_b
-
-                    if (p_b_p - p_b)/p_b_p > (p_a_p - p_a)/p_a_p:
-                        a_star = app_b
-                    else:
-                        a_star = app_a
-        return a_star
+                sl0 = 1 / (self.compute_remaining_time(l, l.tmp_gpus) + EPS)
+                sl1 = 1 / (self.compute_remaining_time(l, l.tmp_gpus+1)+ EPS)
+                sh0 = 1 / (self.compute_remaining_time(h, h.tmp_gpus)+ EPS)
+                sh1 = 1 / (self.compute_remaining_time(h, h.tmp_gpus+1)+ EPS)
 
 
+                if sl0 == 0:
+                    cand = l if sl1 > sh1 else h
+                elif (sl1 - sl0) / sl0 > (sh1 - sh0) / sh1:
+                    cand = l
+                else:
+                    cand = h
+
+
+            cand.tmp_gpus += 1
+
+
+
+            if cand.tmp_gpus == cand.demand:
+                js.remove(cand)
+
+            gpus -= 1
+        
+
+        app_id_to_allocation = {}
+
+        for m in self._active_apps:
+            app_id_to_allocation[m.app_id] = m.tmp_gpus
+        return app_id_to_allocation
+
+
+
+    def compute_remaining_time(self, app, app_current_allocation):
+
+        thrpt = app.jobs[0].thrpt(app_current_allocation)
+
+        if thrpt > 0:
+            
+            # changed here
+            return app.estimated_remaining_service/thrpt
+        return float('inf')
 
     def compute_allocation(self, event_time):
         
@@ -118,24 +126,19 @@ class RayAppAFSScheduler(RayAppGenericScheduler):
 
         residual = min(total_demand, self._max_capacity)
 
-        app_id_to_allocation = {}
-        
-        for app in self._active_apps:
-            app_id_to_allocation[app.app_id] = 0
-
+        app_id_to_allocation = self.alg_c_concept(self._max_capacity)
     
-        while residual > 0:
-
-            app = self.top_priority(app_id_to_allocation)
-
-            allocation = 1 if app_id_to_allocation[app.app_id] < app.demand else 0
-
-            app_id_to_allocation[app.app_id] += allocation
+        for app_id in app_id_to_allocation:
             
+            allocation = app_id_to_allocation[app_id]
+
             residual -= allocation
+            demand = self._app_list[app_id].demand
+
+            assert(allocation <= demand), (allocation, demand, app_id)
 
 
-        assert(np.isclose(residual, 0)), residual
+        assert(math.isclose(residual, 0, abs_tol=1e-3)), residual
 
 
         return app_id_to_allocation
