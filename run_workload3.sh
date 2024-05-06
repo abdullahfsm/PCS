e1214afa6107f59c7914e2154b5ad18c57277562
abdullah <abdullah@cs.tufts.edu>
2024-05-06 13:42:14 -0400

    fix

--- run_workload3.sh
+++ run_workload3.sh
@@ -2,6 +2,10 @@
 
 
 output_files=()
+# THEMIS
+python3 simulation/sim.py -workload gavel -load 1.2 -num_gpus 64 -num_apps 1000 -scheduling_policy THEMIS -output_file THEMIS_gavel_result.csv
+output_files+=(THEMIS_gavel_result.csv)
+
 # FIFO
 python3 simulation/sim.py -workload gavel -load 1.2 -num_gpus 64 -num_apps 1000 -scheduling_policy FIFO -output_file FIFO_gavel_result.csv
 output_files+=(FIFO_gavel_result.csv)
@@ -9,10 +13,6 @@ output_files+=(FIFO_gavel_result.csv)
 # SRSF
 python3 simulation/sim.py -workload gavel -load 1.2 -num_gpus 64 -num_apps 1000 -scheduling_policy SRSF -output_file SRSF_gavel_result.csv
 output_files+=(SRSF_gavel_result.csv)
-
-# THEMIS
-python3 simulation/sim.py -workload gavel -load 1.2 -num_gpus 64 -num_apps 1000 -scheduling_policy THEMIS -output_file THEMIS_gavel_result.csv
-output_files+=(THEMIS_gavel_result.csv)
 
 # AFS
 python3 simulation/sim.py -workload gavel -load 1.2 -num_gpus 64 -num_apps 1000 -scheduling_policy AFS -output_file AFS_gavel_result.csv
