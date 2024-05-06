b29db8943f9d26fb7c6a4aadcef964a5184e2deb
abdullah <abdullah@cs.tufts.edu>
2024-05-06 13:27:02 -0400

    fixed versioning

--- simulation/GenericScheduler.py
+++ simulation/GenericScheduler.py
@@ -545,8 +545,12 @@ class AppGenericScheduler(object):
 
 
             if not ray.is_initialized():
-                ray.init(ignore_reinit_error=True, address="auto")
-
+                if ray.__version__ == '2.0.0.dev0':
+                    ray.init(ignore_reinit_error=True, address="auto")
+                elif ray.__version__ == '2.10.0':
+                    ray.init(ignore_reinit_error=True, address="auto", runtime_env={"env_vars": {"PYTHONPATH": "${PYTHONPATH}:"+f"{os.path.dirname(__file__)}/"}})
+                else:
+                    print("Warning: Incompatible Ray version --- may result in erroneous behaviour")
 
             self._sim_futures = list()
 
