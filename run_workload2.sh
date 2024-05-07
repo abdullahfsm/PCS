28ab254fc76fc16ac2ef0bdd54d2ce8529dc8fde
abdullah <abdullah@cs.tufts.edu>
2024-05-06 22:37:21 -0400

    bug fix

--- run_workload2.sh
+++ run_workload2.sh
@@ -10,7 +10,7 @@ do
 		for var in {jct,bal,pred,}
 		do
 			output_file=new_data/PCS_"$var"_"$trace"_result.csv
-			python3 simulation/sim.py -scheduling_policy MCS -num_gpus 64 -output_file $output_file -trace trace_"$trace" -MCS_config_file data/PCS_configs/PCS_config_"$trace"_avg_jct_avg_pred_error_"$var".pkl
+			python3 simulation/sim.py -scheduling_policy MCS -num_gpus 64 -output_file $output_file -trace trace_"$trace" -MCS_config_file data/PCS_configs/PCS_config_"$trace"_avg_jct_avg_pred_error_"$var".pkl -num_apps -1
 			output_files+=("$output_file")
 			echo "$var" done
 		done
@@ -22,7 +22,7 @@ do
 	for policy in {FIFO,SRSF,AFS,THEMIS,}
 	do
 		output_file=new_data/"$policy"_"$trace"_result.csv
-		python3 simulation/sim.py -scheduling_policy "$policy" -num_gpus 64 -output_file $output_file -trace trace_"$trace"
+		python3 simulation/sim.py -scheduling_policy "$policy" -num_gpus 64 -output_file $output_file -trace trace_"$trace" -num_apps -1
 		output_files+=("$output_file")
 		echo "$policy" done
 	done
