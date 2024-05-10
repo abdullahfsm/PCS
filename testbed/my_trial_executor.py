37d2be3caf091f82be6375030c72d5f9ffd3503d
abdullah <abdullah@cs.tufts.edu>
2024-05-10 09:26:04 -0400

    latest

--- testbed/my_trial_executor.py
+++ testbed/my_trial_executor.py
@@ -167,6 +167,9 @@ class MyRayTrialExecutor(RayTrialExecutor):
                 
                 if trial.trial_id in self._pending:
                     self._pending.pop(trial.trial_id)
+            else:
+                print(f"Unable to start {trial.trial_id} even though enough resources available: {Resources.subtract(self._avail_resources,self._committed_resources)}")
+
 
             return start_val
         
