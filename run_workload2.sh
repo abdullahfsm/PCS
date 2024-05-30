0c999a08aafbae271a8cf5e70fce96aa2da222e4
abdullah <abdullah@cs.tufts.edu>
2024-05-29 22:26:48 -0400

    added timer to script

--- run_workload2.sh
+++ run_workload2.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-traces=()
 
+# Function to start the timer
+start_timer() {
+  start_time=$(date +%s)
+}
+
+# Function to stop the timer and calculate the elapsed time
+stop_timer() {
+  end_time=$(date +%s)
+  elapsed_time=$((end_time - start_time))
+  echo "Script took: $elapsed_time seconds"
+}
+
+
+traces=()
 
 # Set num apps between 100-500 for testing. -1 means the entire trace
 num_apps=-1
+
+# Start the timer
+start_timer
 
 for trace in {0e4a51,ee9e8c,}
 # for trace in {0e4a51,6214e9,6c71a0,b436b2,ee9e8c,}
@@ -25,6 +41,10 @@ do
 	done
 	
 done
+
+# Stop the timer and display the elapsed time
+stop_timer
+
 
 #Compare results
 python3 new_data/parser.py -traces "${traces[@]}"
